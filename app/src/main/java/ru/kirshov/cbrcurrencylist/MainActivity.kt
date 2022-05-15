aeb3684e588f39977d5bf6e8ce4cb464699b63ae
Roma Kirshiv <bloodlordik@yandex.ru>
2022-05-15 13:48:27 +0700

    Initial commit

---
+++ app/src/main/java/ru/kirshov/cbrcurrencylist/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.kirshov.cbrcurrencylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.kirshov.cbrcurrencylist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
